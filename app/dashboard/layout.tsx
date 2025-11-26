8b834e0fb2922e8c7ec8184679c46228014622a4
maytees <mateespublicprofile@gmail.com>
2025-11-26 00:13:56 -0500

    refactor: dashbaord header hiehgt

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="[--header-height:calc(--spacing(14))]">
+    <div className="[--header-height:calc(--spacing(10))]">
       <SidebarProvider className="flex flex-col">
         <SiteHeader />
         <div className="flex flex-1">
