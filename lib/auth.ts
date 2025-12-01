ee25182dc87ef0884f5ea3ecfc3870d0581d830b
maytees <mateespublicprofile@gmail.com>
2025-12-01 17:56:55 -0500

    refactor: sqlite to postgresql

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,7 +14,7 @@ import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "sqlite", // or "mysql", "postgresql", ...etc
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
   emailAndPassword: {
     enabled: true,
